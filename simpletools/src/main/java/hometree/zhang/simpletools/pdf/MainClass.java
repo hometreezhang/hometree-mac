54d77f05970bdd19283e6c851a3514419b4c519b
jiashuzhang <840283619@qq.com>
2023-03-30 17:37:28 +0800

    init

---
+++ simpletools/src/main/java/hometree/zhang/simpletools/pdf/MainClass.java
@@ -0,0 +1,11 @@
+package hometree.zhang.simpletools.pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = "hometree.zhang")
+public class MainClass{
+    public static void main(String[] args) {
+        SpringApplication.run(MainClass.class);
+    }
+}
